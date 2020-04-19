a6a741af90205d37f31ea59b6f0eb64ff38eec25
Joshua Alpert <joshua.alpert1@gmail.com>
2020-04-18 21:00:49 -0700

    Working on the camera controller, right now its pretty broken

--- Assets/Scripts/CamraController.cs
+++ Assets/Scripts/CamraController.cs
@@ -1,24 +1,54 @@
-﻿using System.Collections;
+﻿using Mirror;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CamraController : MonoBehaviour
 {
-    CarController currentPlayer;
+    Transform carTransform;
+    Transform cameraTransform;
+
+    //Publics
+    public Camera playerCamera;
+    public Vector3 followoffset;
+    public float followspeed;
+    public float lookspeed;
+
+
+    public void LookAtPlayer()
+    {
+        //This is jank I have no idea if this will work
+        Vector3 DirectionToPlayer = carTransform.position - cameraTransform.position;
+        Quaternion DirectionDetla = Quaternion.LookRotation(DirectionToPlayer, Vector3.up);
+
+        cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, DirectionDetla, lookspeed * Time.deltaTime);
+    }
+
+    public void MoveToPlayer()
+    {
+        Vector3 PlayerPosition = carTransform.position + carTransform.forward * followoffset.z + carTransform.right * followoffset.x + carTransform.up * followoffset.y;
+
+        cameraTransform.position = Vector3.Lerp(cameraTransform.position, carTransform.position, followspeed * Time.deltaTime);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        currentPlayer = transform.parent.GetComponent<CarController>();
+        carTransform = transform;
+        cameraTransform = playerCamera.GetComponent<Transform>();
+
+        //disable the camera and audio for the server
+        if (!transform.parent.GetComponent<NetworkIdentity>().isLocalPlayer)
+        {
+            gameObject.GetComponent<Camera>().enabled = false;
+            gameObject.GetComponent<AudioListener>().enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!currentPlayer.isLocalPlayer)
-        {
-            gameObject.GetComponent<Camera>().enabled = false;
-            gameObject.GetComponent<AudioListener>().enabled = false;
-        }
+        LookAtPlayer();
+        MoveToPlayer();
     }
 }
